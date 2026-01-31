d7e2a92707883edf84aa9986d0f00e11ed7c8bce
innjd <you@example.com>
2026-01-31 10:53:34 +0200

    settings

--- common/settings.py
+++ common/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ao_vy(-t=@aev)y1q4_(4z_1qgv4i8glma68(#*w3gxpi!()p5'
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-ao_vy(-t=@aev)y1q4_(4z_1qgv4i8glma68(#*w3gxpi!()p5')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEDUG',True)
 
-ALLOWED_HOSTS = ['.onrender.com','127.0.0.1']
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS", '127.0.0.1')]
 
 
 # Application definition
